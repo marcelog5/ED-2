c3d26de906bca7c0bd5e47617317d292fc79b8c4
marcelog5 <marcelovictorg2@gmail.com>
2020-03-17 17:21:11 -0300

    added ex5.c

--- lista-1/ex5.c
+++ lista-1/ex5.c
@@ -1,17 +1,52 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Pilha
+{
+    int num;
+    struct Pilha *proximo;
+} Pilha;
+
+Pilha *empilha(Pilha *pilha, int num)
+{
+    Pilha *novo;
+    novo = (Pilha *)malloc(sizeof(Pilha));
+    novo->num = num;
+    novo->proximo = pilha;
+    pilha = novo;
+    return pilha;
+}
 
 int main(int argc, char const *argv[])
 {
-    int i=0,num,v[1000],lim;
+    int i = 0, num, v[1000000], lim, j, som = 0;
+    Pilha *pilha = NULL, *aux;
 
-    while ((scanf("%d",&num)) == 1 && num != 0)
+    while ((scanf("%d", &num)) == 1 && num != 0)
     {
-        v[i++] = num;
+        v[i] = num;
+        i++;
+    }
+
+    scanf("%d", &lim);
+
+    for (j = 0; j < i; j++)
+    {
+        som += v[j];
+        if (som > lim)
+        {
+            pilha = empilha(pilha, v[j]);
+            som = 0;
+        }
     }
     
-    scanf("%d",&lim);
-
-    
+    while (pilha != NULL)
+    {
+        printf("%d\n",pilha->num);
+        aux = pilha;
+        pilha = pilha->proximo;
+        free(aux);
+    }
 
     return 0;
 }
